123302871428639ca22528321db6c6b63f13337c
Syed Omair <syedkhalidomair@gmail.com>
2017-03-05 20:15:30 +0500

    changed main

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gokit_micro
--- main.go
+++ main.go
@@ -1,50 +1,114 @@
 package main
 
 import (
-	_ "expvar"
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
-	"os"
-	"time"
+	"strings"
 
 	"github.com/go-kit/kit/endpoint"
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/metrics/expvar"
-	kitratelimit "github.com/go-kit/kit/ratelimit"
 	httptransport "github.com/go-kit/kit/transport/http"
-	"github.com/juju/ratelimit"
-	"golang.org/x/net/context"
 )
 
+// StringService provides operations on strings.
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
 func main() {
-	logger := log.NewLogfmtLogger(os.Stdout)
-
 	ctx := context.Background()
+	svc := stringService{}
 
-	c := &countService{}
-
-	var svc endpoint.Endpoint
-	svc = makeAddEndpoint(c)
-
-	limit := ratelimit.NewBucket(2*time.Second, 1)
-	svc = kitratelimit.NewTokenBucketLimiter(limit)(svc)
-
-	requestCount := expvar.NewCounter("request.count")
-	svc = metricsMiddleware(requestCount)(svc)
-	//svc = loggingMiddlware(logger)(svc)
-
-	addHandler := httptransport.NewServer(
+	uppercaseHandler := httptransport.NewServer(
 		ctx,
-		svc,
-		decodeAddRequest,
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
 		encodeResponse,
-		httptransport.ServerBefore(beforeIDExtractor, beforePATHExtractor),
 	)
 
-	http.Handle("/add", addHandler)
+	countHandler := httptransport.NewServer(
+		ctx,
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
 
-	port := os.Getenv("PORT")
-	logger.Log("listening on", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		logger.Log("listen.error", err)
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
 	}
 }
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
+}
+
+func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request countRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+// ErrEmpty is returned when an input string is empty.
+var ErrEmpty = errors.New("empty string")
