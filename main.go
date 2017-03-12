adb024b35bc0a935f53314d8e8065f726b35a8eb
Syed Omair <syedkhalidomair@gmail.com>
2017-03-12 16:22:59 +0500

    added dependcy injection

--- main.go
+++ main.go
@@ -1,29 +1,73 @@
 package main
 
 import (
-    "context"
-    "net/http"
-    "os"
-    "github.com/go-kit/kit/log"
-    httptransport "github.com/go-kit/kit/transport/http"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/go-kit/kit/log"
+	"net/http"
+	"os"
 )
 
+type Env struct {
+	db *sql.DB
+}
+
 func main() {
-    ctx := context.Background()
-    logger := log.NewLogfmtLogger(os.Stderr)
+	logger := log.NewLogfmtLogger(os.Stderr)
+	db, _ := DBService()
+	env := &Env{db: db}
+	http.Handle("/public/books", booksIndex(env))
+	port := os.Getenv("PORT")
+	logger.Log("msg", "HTTP", "addr", ":"+port)
+	logger.Log("err", http.ListenAndServe(":"+port, nil))
+}
 
-    var service BookService
-    service = bookService{}
+func booksIndex(env *Env) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, http.StatusText(405), 405)
+			return
+		}
+		bks, _ := PublicBooks(env.db)
+		js, err := json.Marshal(bks)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-    publicBookHandler := httptransport.NewServer(
-        ctx,
-        makePublicBooksEndpoint(service),
-        decodeRequest,
-        encodeResponse,
-    )
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
+}
 
-    http.Handle("/public/books", publicBookHandler)
-    port := os.Getenv("PORT")
-    logger.Log("msg", "HTTP", "addr", ":"+port)
-    logger.Log("err", http.ListenAndServe(":"+port, nil))
+func PublicBooks(db *sql.DB) ([]BookResponse, error) {
+
+	rows, err := db.Query("SELECT book.id, " +
+		" book.user_id, " +
+		" u.first_name, " +
+		" u.last_name, " +
+		" book.name, " +
+		" book.description, " +
+		" book.publish " +
+		" FROM book join public.user as u " +
+		" on book.user_id = u.id  " +
+		" WHERE book.publish = true  ")
+	if err == sql.ErrNoRows {
+		fmt.Println("No Results Found")
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+	books := make([]BookResponse, 0)
+	for rows.Next() {
+		book := BookResponse{}
+		err := rows.Scan(&book.Id, &book.UserId, &book.FirstName, &book.LastName, &book.Name, &book.Description, &book.Publish)
+		if err != nil {
+			fmt.Println(err)
+		}
+		books = append(books, book)
+	}
+	return books, nil
 }
--- transport.go
+++ transport.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "net/http"
-    "github.com/go-kit/kit/endpoint"
+	"context"
+	"encoding/json"
+	"github.com/go-kit/kit/endpoint"
+	"net/http"
 )
 
 func makePublicBooksEndpoint(bookService BookService) endpoint.Endpoint {
-    return func(ctx context.Context, request interface{}) (interface{}, error) {
-        return bookService.PublicBooks(), nil
-    }
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		return bookService.PublicBooks(), nil
+	}
 }
 
 func decodeRequest(_ context.Context, r *http.Request) (interface{}, error) {
-    return "", nil
+	return "", nil
 }
 
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
-    return json.NewEncoder(w).Encode(response)
+	return json.NewEncoder(w).Encode(response)
 }
